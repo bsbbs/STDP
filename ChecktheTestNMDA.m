a5d515d3f6b06062adaff565822585e6660ca0ad
Bo Shen <40451542+bsbbs@users.noreply.github.com>
2024-07-18 17:02:56 -0400

    Examine Training Effects

--- ChecktheTestNMDA.m
+++ ChecktheTestNMDA.m
@@ -14,7 +14,69 @@ Ntwkfile = fullfile(plotdir, 'Ntwk.mat');
 load(Ntwkfile);
 %% visualizing dynamically changing periods
 smplonsets = round(evs(round(linspace(1,numel(evs(:,1)),18)),1)*1000/dt);
-for evi = 15:18%numel(smplonsets)
+EIMtrx = nan(numel(smplonsets)+1, Ntwk.Input.Source);
+IEMtrx = nan(numel(smplonsets)+1,  Ntwk.Input.Source);
+RIMtrx = nan(numel(smplonsets)+1, Ntwk.Input.Source, Ntwk.Input.Source);
+for evi = 0:numel(smplonsets)
+    if evi == 0
+        WEI = Ntwk.wEI_initial;
+        WIE = Ntwk.wIE_initial;
+        WEE = Ntwk.wEE_initial;
+    else
+        filename = sprintf('RealtimeMonitor_Event%i', evi);
+        load(fullfile(plotdir, [filename, '.mat']));
+    end
+    InputtoI = WEI*Ntwk.Cnnct_Input;
+    ItoInput = Ntwk.Cnnct_Input'*WIE;
+    RecurrentInhbt = ItoInput*InputtoI;
+
+    for i = 1:Ntwk.Input.Source
+        EIMtrx(evi+1,i) = mean(sum(InputtoI(:,Ntwk.Input.Origins == i), 1));
+        IEMtrx(evi+1,i) = mean(sum(ItoInput(Ntwk.Input.Origins == i,:), 2));
+    end
+    for i = 1:Ntwk.Input.Source % to
+        for j = 1:Ntwk.Input.Source % from
+            RIMtrx(evi+1,i,j) = mean(RecurrentInhbt(Ntwk.Input.Origins == i, Ntwk.Input.Origins == j), 'all');
+        end
+    end
+end
+%% 
+h = figure;
+filename = 'WeightsSummaryDynamics';
+subplot(2,2,1); hold on;
+for i = 1:2
+    plot(0:18,EIMtrx(:,i), 'Color',OKeeffe(i,:));
+end
+legend({'E to I from Input 1','E to I from Input 2'}, 'Location','best');
+xlabel('Check time point');
+ylabel('Summed weights');
+mysavefig(h, filename, plotdir, 12, [5, 5], 1);
+subplot(2,2,2); hold on;
+for i = 1:2
+    plot(0:18,IEMtrx, 'Color',OKeeffe(i,:));
+end
+legend({'I to E to Input 1','I to E to Input 2'}, 'Location','best');
+xlabel('Check time point');
+ylabel('Summed weights');
+mysavefig(h, filename, plotdir, 12, [5, 5], 1);
+subplot(2,2,3); hold on;
+for i = 1:2
+    plot(0:18,RIMtrx(:,i,1), 'Color',OKeeffe(i,:));
+end
+legend({'Input 1 to Input 1','Input 1 to Input 2'}, 'Location','best');
+xlabel('Check time point');
+ylabel('Summed weights');
+mysavefig(h, filename, plotdir, 12, [5, 5], 1);
+subplot(2,2,4); hold on;
+for i = 1:2
+    plot(0:18,RIMtrx(:,i,2), 'Color',OKeeffe(i,:));
+end
+legend({'Input 2 to Input 1','Input 2 to Input 2'}, 'Location','best');
+xlabel('Check time point');
+ylabel('Summed weights');
+mysavefig(h, filename, plotdir, 12, [5, 5], 1);
+%%
+for evi = 1:18%numel(smplonsets)
     filename = sprintf('RealtimeMonitor_Event%i', evi);
     load(fullfile(plotdir, [filename, '.mat']));
     plotdirintersect = fullfile(plotdir, sprintf('Intersect%i', evi));
@@ -109,3 +171,4 @@ for evi = 15:18%numel(smplonsets)
     %%
     % EvalTuning(Ntwk,WEE,WEI,WIE,OKeeffe,plotdirintersect);
 end
+
