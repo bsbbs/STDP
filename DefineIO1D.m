05594cf40aa8c3efeede41822481494d24d91a55
Bo Shen <40451542+bsbbs@users.noreply.github.com>
2024-12-18 17:58:14 -0500

    Create DefineIO1D.m

---
+++ DefineIO1D.m
@@ -0,0 +1,18 @@
+[os, ~, ~] = computer;
+if strcmp(os,'MACI64')
+    Projdir = '/Users/bs3667/Dropbox (NYU Langone Health)/Bo Shen Working files/STDP_Project/Analyses';
+    Gitdir = '~/STDP';
+elseif strcmp(os,'GLNXA64')
+    % Projdir = '/gpfs/data/glimcherlab/BoShen/STDP_Project';
+    % Gitdir = '/gpfs/data/glimcherlab/BoShen/STDP';
+    Projdir = '/scratch/bs3667/STDP_Project/Analyses';
+    Gitdir = '/home/bs3667/STDP';
+elseif strcmp(os, 'PCWIN64')
+    Projdir = 'C:\Users\Bo\NYU Langone Health Dropbox\Shen Bo\Bo Shen Working files\STDP_Project\Analyses';
+    Gitdir = 'C:\Users\Bo\Documents\GitHub\STDP';
+end
+gnrloutdir = fullfile(Projdir, 'General1DSmallerPatch');
+if ~exist(gnrloutdir,'dir')
+    mkdir(gnrloutdir);
+end
+addpath(genpath(Gitdir));
